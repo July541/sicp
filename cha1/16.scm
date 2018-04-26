e3ab649bf769c7c84b74a4952721a818658b80ef
July Treee <julytreee@NaiHe.local>
2018-04-27 00:17:52 +0800

    Revert "add 17, 18, 19"
    This reverts commit 08673cc6e475fdebe8cd3b34328e71b16d794fb0.

--- cha1/16.scm
+++
@@ -1,9 +0,0 @@
-; Exercise 1.16.
-
-(define (expt b n)
-    (expt-iter 1 b n))
-
-(define (expt-iter a b n)
-    (cond ((= n 0) a)
-          ((odd? n) (expt-iter (* a b) b (-1+ n)))
-          ((even? n) (expt-iter a (square b) (/ n 2)))))
