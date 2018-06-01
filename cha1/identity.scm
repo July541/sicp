116a40dafebc140d5d612ed98845c1b556828c66
July541 <zhulei1995@126.com>
2018-06-01 22:23:11 +0800

    resolve identify

--- cha1/identity.scm
+++ cha1/identity.scm
@@ -1,11 +1,4 @@
 ; Return itself of n
 
 (define (identity n)
-    (lambda (x)
-        (/ (+ (f (- x dx))
-              (f x)
-              (f (+ x dx)))
-        3)))
-
-(define (smooth-ntimes f times)
-    ((repeat smooth times) f))
+    n)
